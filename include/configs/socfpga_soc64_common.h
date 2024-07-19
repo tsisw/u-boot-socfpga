1fca11dc0ac545faa052710dbffd4365db09cc60
Vipin Garg <vgarg@fpga1.tsavoritesi.net>
2024-07-19 12:06:33 -0700

    TSI: fixed kernel bootup issues

--- include/configs/socfpga_soc64_common.h
+++ include/configs/socfpga_soc64_common.h
@@ -204,8 +204,12 @@
 		" else fdt set /clocks/qspi-clk clock-frequency" \
 		" ${qspi_clock}; echo QSPI clock frequency updated; fi; fi\0" \
 	"scriptaddr=0x05FF0000\0" \
-	"scriptfile=boot.scr\0" \
+	"scriptfile=u-boot.scr\0" \
 	"nandroot=ubi0:rootfs\0" \
+	"nandfitboot=setenv bootargs " CONFIG_BOOTARGS \
+			" root=${nandroot} rw rootwait rootfstype=ubifs ubi.mtd=1; " \
+			"bootm ${loadaddr}\0" \
+	"nandfitload=ubi part root; ubi readvol ${loadaddr} kernel\0" \
 	"socfpga_legacy_reset_compat=1\0" \
 	"rsu_status=rsu dtb; rsu display_dcmf_version; "\
 		"rsu display_dcmf_status; rsu display_max_retry\0" \
